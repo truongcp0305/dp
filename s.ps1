d8abd0ca0960adec378aa1d9123630b7af3e4a8e
truong.nguyen <nguyentruongcp35@gmai.com>
2025-10-15 22:24:52 +0700

    feat: add execution policy setting to s.ps1 for improved script execution

--- s.ps1
+++ s.ps1
@@ -1,3 +1,5 @@
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
 $scriptPath = "$PSScriptRoot\temp_2.ps1"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/truongcp0305/dp/main/2.ps1" -OutFile $scriptPath
 

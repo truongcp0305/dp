4f9785ab8223ba5c0a14e28f6b8f50fbf1dbe521
truong.nguyen <nguyentruongcp35@gmail.com>
2025-11-14 09:51:32 +0700

    c

--- 1.ps1
+++ 1.ps1
@@ -2,15 +2,8 @@ Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
 $scriptDir = if ($PSScriptRoot) { $PSScriptRoot } else { Split-Path -Parent $MyInvocation.MyCommand.Definition }
 
-$scriptPath = Join-Path $scriptDir "2.ps1"
-
-# Dùng WebClient thay vì Invoke-WebRequest để tránh lỗi "Cannot find drive"
-try {
-    $wc = New-Object System.Net.WebClient
-    $wc.DownloadFile("https://raw.githubusercontent.com/truongcp0305/dp/main/2.ps1", $scriptPath)
-} finally {
-    if ($wc) { $wc.Dispose() }
-}
+$scriptPath = "$scriptDir\2.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/truongcp0305/dp/main/2.ps1" -OutFile $scriptPath
 
 & $scriptPath
 
